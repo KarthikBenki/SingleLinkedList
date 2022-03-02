35d67d88e4aad4d99e457c553ef230eec5a2c50b
karthikbenki <karthikmc007@gmail.com>
2022-03-02 10:12:57 +0530

    Added: Ability to insert element

--- src/Javalinkedlist/MyLinkedListTest.java
+++ src/Javalinkedlist/MyLinkedListTest.java
@@ -5,13 +5,14 @@ public class MyLinkedListTest {
         MyNode myFirstNode = new MyNode<Integer>(56);
         MyNode mySecondNode = new MyNode<Integer>(30);
         MyNode myThirdNode = new MyNode<Integer>(70);
+        MyNode myFourthNode = new MyNode<Integer>(40);
 
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.append(myFirstNode);
         myLinkedList.append(mySecondNode);
         myLinkedList.append(myThirdNode);
+        myLinkedList.insert(mySecondNode,myFourthNode);
         myLinkedList.printNodes();
-        INode tempnode = new MyNode(30);
-        myLinkedList.search(tempnode);
+
     }
 }
